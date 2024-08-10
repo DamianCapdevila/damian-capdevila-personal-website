f3fbc2725980a9ffcba53fa4092265c816eadae7
Damian Capdevila <damian.capdevila@hotmail.com>
2024-08-10 16:36:08 +0200

    (feat): provide more context in system message and limit the length of the accepted user prompt.

--- src/pages/chat.py
+++ src/pages/chat.py
@@ -2,8 +2,9 @@ from forms.contact import contact_form
 from utilities.gpt_communication import get_gpt4o_response
 import streamlit as st
 
+MAX_MESSAGE_LENGTH = 200  
 
-st.title("💬 Ask anything about me!")
+st.title("💬 Ask anything about Damian!")
 st.caption("🚀 I know Damian Capdevila very well. Feel free to ask anything about him!")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
@@ -12,14 +13,17 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input(placeholder="Tell me something about Damian Capdevila"):
-    full_response = ""
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = get_gpt4o_response(st.session_state.messages)
-    msg = response
-    with st.chat_message("assistant"):
-        full_response = st.write_stream(msg)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    if len(prompt) > MAX_MESSAGE_LENGTH:
+        st.warning(f"Your message is too long. Please limit your message to {MAX_MESSAGE_LENGTH} characters.")
+    else:
+        full_response = ""
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        response = get_gpt4o_response(st.session_state.messages)
+        msg = response
+        with st.chat_message("assistant"):
+            full_response = st.write_stream(msg)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 # Call to Action - Contact Me
 st.write("---")
--- src/utilities/gpt_communication.py
+++ src/utilities/gpt_communication.py
@@ -11,6 +11,20 @@ cv_content = file_handler.load_cvs_from_folder(file_handler.assets_folder_path)
 SYSTEM_MESSAGE = f"""
         You are an AI chatbot that provides responses based on the following CVs:
         {cv_content}
+
+        If the user requests contact information, answer based on the following information:
+        Linkedin: https://linkedin.com/in/damiancapdevila
+        Phone number: +34 673 47 82 70
+        Email address: damian.capdevila@hotmail.com
+        Blog: https://substack.com/home/post/p-144238956?r=3cvmsg&utm_campaign=post&utm_medium=web
+        GitHub: https://github.com/damiancapdevila
+
+        Additionally, tell the user that Damian has the legal right to work in every EU country and also in Argentina.
+
+        Also, if the user asks for services provided by Damian, answer based on the following:
+        Damian Capdevila provides consultancy services for transitioning into software development, Resume and Linkedin profile review and
+        enhancements, international relocation advice, and interview preparation. 
+        Point the user to Damian's contact information.
         """
 
 # Function to get a response from GPT-4o based on Damian CVs. We consider the last 10 messages for context.
@@ -20,7 +34,7 @@ def get_gpt4o_response(messages):
         modifiedMessages = copy.deepcopy(messages)
         modifiedMessages.insert(len(messages) - 1, predefinedSystemMessage)
         response = openai.chat.completions.create(
-            model="gpt-4o",
+            model="gpt-4o-mini",
             messages=modifiedMessages[-10:],
             max_tokens=1500,
             temperature=0.7,
