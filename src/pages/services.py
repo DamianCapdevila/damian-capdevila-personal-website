d4f33720fb04465d91b39c62bcc0bd0606e9dfa8
Damian Capdevila <damian.capdevila@hotmail.com>
2024-08-21 11:43:16 -0400

    (feat): create services page

--- src/pages/services.py
+++ src/pages/services.py
@@ -1,32 +1,69 @@
 import streamlit as st
 
-st.title("Services")
+# URLs, durations, and prices for booking services
+INITIAL_CONSULTATION_URL = "https://cal.com/damiancapdevila/initial-consultation"
+INITIAL_CONSULTATION_DURATION = "45"
+INITIAL_CONSULTATION_PRICE = "50"
 
-# HTML content with the Stripe pricing table
-html_content = """
-<!DOCTYPE html>
-<html lang="en">
-<script async
-  src="https://js.stripe.com/v3/buy-button.js">
-</script>
+RESUME_REVIEW_URL = "https://cal.com/damiancapdevila/resume-review-writing"
+RESUME_REVIEW_DURATION = "45"
+RESUME_REVIEW_PRICE = "50"
 
-<stripe-buy-button
-  buy-button-id="buy_btn_1PnKa400vggv2sUBuGVejI7l"
-  publishable-key="pk_live_51PnIm700vggv2sUBL882Mfg4Mz41ZRhVL8dsESZym4FPG0YOn5UEzaD4qoGLvBdgoKuYiSFdyiE0jbmhKZq2HHdi00LgSLT3ht"
->
-</stripe-buy-button>
+REGULAR_CONSULTATION_URL = "https://cal.com/damiancapdevila/regular-consultation"
+REGULAR_CONSULTATION_DURATION = "60"
+REGULAR_CONSULTATION_PRICE = "100"
 
-<script async
-  src="https://js.stripe.com/v3/buy-button.js">
-</script>
+CONTAINER_HEIGHT = 498
 
-<stripe-buy-button
-  buy-button-id="buy_btn_1PnKpJ00vggv2sUB9VdrpSrz"
-  publishable-key="pk_live_51PnIm700vggv2sUBL882Mfg4Mz41ZRhVL8dsESZym4FPG0YOn5UEzaD4qoGLvBdgoKuYiSFdyiE0jbmhKZq2HHdi00LgSLT3ht"
->
-</stripe-buy-button>
-</html>
-"""
+st.title("My Services")
+col1, col2 = st.columns(2, gap='small')
 
-# Use Streamlit's HTML component to render the HTML content
-st.components.v1.html(html_content, height = 1000, width = 1000)
+# Initial Consultation
+with col1:
+    with st.container(height=CONTAINER_HEIGHT, border=True):
+        st.subheader("**Initial Consultation**", anchor=False)
+        st.write(f"⏳ Duration: {INITIAL_CONSULTATION_DURATION} minutes")
+        st.write(f"💶 Price: {INITIAL_CONSULTATION_PRICE} Euros")
+        st.write("""
+        **🔎 What We Do:**
+        - Discuss your background, experience, and career goals.
+        - Assess your strengths and areas to focus on for your transition to software engineering.
+        - Define a roadmap for your transition.
+                 
+        **Special Offer:** Free for the first five customers to help you get started without any cost.
+        """)
+        st.link_button("BOOK INITIAL CONSULTATION", type="primary", use_container_width=True, url=INITIAL_CONSULTATION_URL)
+
+# Resume Review/Writing
+with col2:
+    with st.container(height=CONTAINER_HEIGHT, border=True):
+        st.subheader("**Resume Review/Writing**", anchor=False)
+        st.write(f"⏳ Duration: {RESUME_REVIEW_DURATION} minutes")
+        st.write(f"💶 Price: {RESUME_REVIEW_PRICE} Euros")
+        st.write(f"""
+        **📄 What We Do:**
+        - Thoroughly review your current resume with feedback tailored for software engineering roles.
+        - Help craft a new resume that emphasizes your transferable skills and experience.
+        - Make your CV ATS-proof.
+        
+        **Special Offer:** Free for the first five customers to help you get started without any cost.
+        """)
+        st.link_button("BOOK RESUME REVIEW", type="primary", use_container_width=True, url=RESUME_REVIEW_URL)
+
+col3, col4 = st.columns(2, gap='small')
+
+# Regular Consultation
+with col3:
+    with st.container(height=CONTAINER_HEIGHT, border=True):
+        st.subheader("**Regular Consultation**", anchor=False)
+        st.write(f"⏳ Duration: {REGULAR_CONSULTATION_DURATION} minutes")
+        st.write(f"💶 Price: {REGULAR_CONSULTATION_PRICE} Euros")
+        st.write("""
+        **📈 What We Do:**
+        - Provide ongoing support after the initial consultation.
+        - Assess your current transition status.
+        - Assist with coding challenges, interview preparation, and career development planning.
+        - Get access to all support material to help your transition.
+        """)
+        st.write("")
+        st.link_button("BOOK REGULAR CONSULTATION", type="primary", use_container_width=True, url=REGULAR_CONSULTATION_URL)
