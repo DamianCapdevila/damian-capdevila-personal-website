fa5a5148a8a740d79e14172d581d6896f10dcf91
Damian Capdevila <damian.capdevila@hotmail.com>
2024-09-11 19:39:10 +0200

    (refactor): clean test

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -4,15 +4,8 @@ import pytest
 from forms.contact import contact_form
 from utilities.footer import footer
 
-# Add src directory to Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
-
-
 def test_contact_form():
-    # Check if the contact_form function is callable
     assert callable(contact_form)
 
 def test_footer():
-    # Check if the footer function is callable
     assert callable(footer)
