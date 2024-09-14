55822e4e10f78a801eca41e88fff0e054e9d300b
Damian Capdevila <damian.capdevila@hotmail.com>
2024-09-14 15:03:06 +0200

    (feat): improve UVP and streamline experience section

--- src/pages/about.py
+++ src/pages/about.py
@@ -3,16 +3,17 @@ from forms.contact import contact_form
 from utilities.footer import footer
 
 HEADSHOT_URL = 'https://raw.githubusercontent.com/DamianCapdevila/damian-capdevila-personal-website-assets/main/Damian-sin%20fondo-gris.png'
+UVP_TEXT = f"**I help engineers Unlock the Freedom, Flexibility, and Higher Income of a Software Career!**"
+UVP_EXPLANATION = f"**Thrive in the digital age** by **transforming your engineering experience** into a Software Engineering Powerhouse, **without the fear or uncertainty.**"
 
 col1, col2 = st.columns(2, gap='small', vertical_alignment="top")
 
 with col1:
     st.image(HEADSHOT_URL, output_format="PNG", width=300)
 with col2:
-    
-    
-    st.title("Damian Capdevila", anchor=False)
-    st.write("**Software Engineer** - Empowering Engineers to Transition into Software Development with Confidence and Precision!")
+    st.title("Damián Capdevila", anchor=False)
+    st.write(UVP_TEXT)
+    st.write(f"*{UVP_EXPLANATION}*")
         
 st.subheader("Providing Services", divider=True)
 st.write("*Are you ready to start your transition into software engineering?*")
@@ -40,7 +41,7 @@ with st.expander(label="**Skills**", icon="🛠"):
     - **Presales:** *Technical Consultancy, Conference Speaking, Professional English, Native Spanish*  
     - **Programming:** *C#, .NET, REST API, WPF, Blazor, MVVM, Unity Container, Nunit, Python, Streamlit*  
     - **Industrial Automation:** *PLC, HMI, Drives, Commissioning*  
-    - **Tools:** *Docker, Git, Azure, Visual Studio, VSCode, Azure DevOps Platform*   
+    - **Tools:** *Docker, Git, Postman, Azure, Visual Studio, VSCode, Azure DevOps Platform*   
     - **Methodologies:** *Agile Software Development, Refactoring Legacy Systems, Code Reviewing* 
     """)
 
@@ -49,7 +50,7 @@ with st.expander(label="**Experience**", icon="👨‍🏭"):
     with st.container(border=True):
         st.write("**📋 Summary**")
         st.write("""
-                    Greetings! I'm Damián Capdevila, an accomplished electronic engineer with **seven years of experience spanning software development, 
+                    Greetings 👋🏻! I'm Damián Capdevila, an accomplished electronic engineer with **seven years of experience spanning software development, 
                     solutions engineering, presales, and academic research**. My current focus is on **engineering enterprise-grade software using C# .NET, 
                     serving a global user base.**
 
@@ -60,55 +61,8 @@ with st.expander(label="**Experience**", icon="👨‍🏭"):
     
     with st.container(border=True):
         st.write("**🔎 Details**",)
-        st.write("""
-        **Presales Software Engineer - Siemens**  
-        *Madrid, Spain (11/2023 - Current)*  
-        - *[Drove success of a multi-billion dollar project](https://www.linkedin.com/posts/damiancapdevila_after-3-weeks-on-site-in-korea-giving-activity-7119254288891047936-Kd5a?utm_source=share&utm_medium=member_desktop) by achieving 100% faster machine-machine communication, providing engineering consultancy for the world's largest battery production project in South Korea (PLC programming, TIA Portal, Profinet).* 
-        
-        - *Led the design and development of an enterprise-grade application used worldwide across 150 factories by an automotive brand, saving ~80k Euros quarterly by speeding up the SDLC by 400%(C#, .NET Framework 4.8, MVVM, WPF, Unity Container for DI)*.
-                
-        - *Delivered 10+ features in 2 months for an RAG AI assistant, saving ~60k Euros in operation costs monthly (C#, .NET 8, MVVM, RAG, WPF, NUnit, Azure OpenAi Studio, Streamlit, Python).*
-                
-        - *Collected over thirty business leads while overseeing the exhibition of a standardization use case at [Hannover Messe](https://www.hannovermesse.de/en/)*.
-        
-        - *Presented at the Siemens Technical Consultants Global Conference, sharing expertise in TIA Portal Openness API with 80 attendees.*
-        
-        """)
-
-        st.write("""
-        **Application Engineer - Siemens**  
-        *Plzen, Czech Republic (07/2020 - 11/2023)*  
-        - *Generated ~100k Euros by solving support requests and providing presales consultancy to Siemens’ branches worldwide.*
-        
-        - *Created a [motion control application example](https://www.youtube.com/watch?v=K2I3nb9-okg) with a technical manual that received over 11k views.*
-        
-        - *Coordinated the creation of a Motion Control Web-Based training, acting as a SME and curating the material.*
-        """)
-
-        st.write("""
-        **Service Engineer - Siemens**  
-        *La Plata, Argentina (05/2019 - 07/2020)*  
-        - *Managed tens of contractors, ensuring quality and timeliness of their work, contributing to TecPlata’s success.*
-                
-        - *Saved TecPlata ~150k USD by reducing downtimes during ship load/unload operations.*
-        """)
-
-        st.write("---")
-        st.write("""
-        **Doctoral Researcher - National University of La Plata**  
-        *La Plata, Argentina (05/2018 - 05/2019)*  
-        - *Published ["Wearable platform for Human-Machine interfaces"](https://sedici.unlp.edu.ar/bitstream/handle/10915/75309/Documento_completo.pdf-PDFA.pdf?sequence=1) focusing on hardware and software design (C#, Altium Designer).*
-        """)
-
-        st.write("---")
-        st.write("""
-        **Software Engineer - A.R.G SRL**  
-        *La Plata, Argentina (10/2017 - 05/2018)*  
-        - *Designed and programmed a desktop app for real-time waste treatment management for 1000+ hospitals. Generating over 100k USD in revenue (VB.NET + WinForms + Microsoft SQL)*
-        """)
-
+        st.write("Visit my [LinkedIn Profile](https://www.linkedin.com/in/damiancapdevila)")
         st.write("")
-    st.caption("**Disclaimer**: *All monetary estimates are based on a personal calculation. Please note that companies estimates might differ.*")
 
 with st.expander("**Education**", icon="👨‍🎓"):
     st.subheader("Formal Education", divider=True)
