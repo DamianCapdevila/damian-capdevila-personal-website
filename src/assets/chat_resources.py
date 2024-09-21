bcb6b760313f6aff7e7ab346f0f107037983f5f7
Damian Capdevila <damian.capdevila@hotmail.com>
2024-09-21 14:55:06 +0200

    (refactor): make all the strings in the chat a resource.

--- src/assets/chat_resources.py
+++ src/assets/chat_resources.py
@@ -1,1 +1,12 @@
 MAX_MESSAGE_LENGTH = 200
+
+# Titles and Captions
+CHAT_TITLE = "💬 Ask anything about Damian!"
+CHAT_CAPTION = "👂Psst... Did you know that Damian is providing services?"
+
+# Prompts and Warnings
+PROMPT_PLACEHOLDER = "What services does Damian provide?"
+MESSAGE_TOO_LONG_WARNING = "Your message is too long. Please limit your message to {} characters."
+INITIAL_ASSISTANT_MESSAGE = "How can I help you?"
+MAX_USER_MESSAGES = 6
+MAX_USER_MESSAGES_WARNING = "You have reached the maximum number of user messages ({MAX_USER_MESSAGES})."
--- src/pages/chat.py
+++ src/pages/chat.py
@@ -1,31 +1,37 @@
 from utilities.gpt_communication import get_gpt4o_response
 import utilities.helper
 import streamlit as st
+from assets.chat_resources import *
 
-MAX_MESSAGE_LENGTH = 200  
-
-st.title("💬 Ask anything about Damian!")
-st.caption("👂Psst... Did you know that Damian is providing services?")
+st.title(CHAT_TITLE)
+st.caption(CHAT_CAPTION)
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": INITIAL_ASSISTANT_MESSAGE}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="What services does Damian provide?"):
-    if len(prompt) > MAX_MESSAGE_LENGTH:
-        st.warning(f"Your message is too long. Please limit your message to {MAX_MESSAGE_LENGTH} characters.")
-    else:
-        full_response = ""
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.chat_message("user").write(prompt)
-        response = get_gpt4o_response(st.session_state.messages)
-        msg = response
-        with st.chat_message("assistant"):
-            full_response = st.write_stream(msg)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
+# Count the number of user messages
+user_message_count = sum(1 for msg in st.session_state.messages if msg["role"] == "user")
+
+if user_message_count < MAX_USER_MESSAGES:
+    if prompt := st.chat_input(placeholder=PROMPT_PLACEHOLDER):
+        if len(prompt) > MAX_MESSAGE_LENGTH:
+            st.warning(MESSAGE_TOO_LONG_WARNING.format(MAX_MESSAGE_LENGTH))
+        else:
+            full_response = ""
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.chat_message("user").write(prompt)
+            response = get_gpt4o_response(st.session_state.messages)
+            msg = response
+            with st.chat_message("assistant"):
+                full_response = st.write_stream(msg)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+else:
+    st.warning(MAX_USER_MESSAGES_WARNING)
 
 if len(st.session_state.messages) > 2:
+
     utilities.helper.call_to_action()
 
 
